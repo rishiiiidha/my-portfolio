4f20082af738aafedc296bacd4d7695462133412
rishiiiidha <rishidha04@gmail.com>
2024-07-17 18:33:15 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import DesignerProfile from './components/AboutMe'
+import Navbar from './components/Navbar'
+import RecentWork from './components/RecentWork'
+import SocialLinks from './components/SocialLinks'
+import AIProductCard from './components/AIProductCard'
+import Developer from './components/Developer'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+<div className="grid grid-cols-10 grid-rows-23 gap-2 md:w-3/5 m-auto">
+    <div className="col-span-10 "><Navbar/></div> 
+    {/* // navbar */}
+    <div className="col-span-10 row-span-5 row-start-2"><DesignerProfile/></div> 
+    {/* // profile */}
+    <div className="col-span-5 row-span-4 row-start-7"><RecentWork/></div>  
+    {/* //recent work experience */}
+    <div className="col-span-5 row-span-2 col-start-6 row-start-7"><Developer/></div>  
+    {/* // social links */}
+    <div className="col-span-3 row-span-2 col-start-6 row-start-9"><Developer/></div>  
+    {/* //coding links */}
+    <div className="col-span-10 row-span-2 row-start-11">Tech stack scroll</div>
+     {/*Techstack scroll  */}
+    <div className="col-span-6 row-span-7 row-start-13"><Developer/></div>
+     {/* //project1 */}
+    <div className="col-span-4 row-span-4 col-start-7 row-start-13"><Developer/></div>
+     {/* //project2 */}
+    <div className="col-span-4 row-span-3 col-start-7 row-start-17"><Developer/></div> 
+    {/* //project3 */}
+    <div className="col-span-6 row-span-3 row-start-20"><Developer/></div> 
+    {/* // Achievements */}
+    <div className="col-span-4 row-span-2 col-start-7 row-start-20"><Developer/></div> 
+    {/* //colors-shift -change */}
+    <div className="col-span-4 col-start-7 row-start-22"><Developer/></div>  
+    {/* //blog */}
+    <div className="col-span-10 row-start-23"><Navbar/></div> 
+      {/* // Footer */}
+</div>
+    
   )
 }
 
