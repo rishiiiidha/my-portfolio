4d290f1c2146a76e5906740e6eb7b22fc2c5e0ab
rishiiiidha <rishidha04@gmail.com>
2024-07-17 16:39:40 +0530

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>index</div>
+    <div className="flex w-[1328px]">
+    <div className="w-[64px] h-[64px] border-2 border-[#262626] bg-[#1B1B1B] rounded-full ">
+      <img src="" alt=""/>
+    </div>
+      <div className="flex bg-[#1B1B1B] text-white w-[1152px] h-[64px] justify-between items-center rounded-full px-6 border-2 border-[#262626]">
+      <div className="uppercase font-bold text-[#F35034]">
+        About
+      </div> 
+      <div className="uppercase">
+       Resume
+      </div>
+      <div className="uppercase">
+       Contact
+      </div>
+    </div>
+  </div>
   )
 }
 
