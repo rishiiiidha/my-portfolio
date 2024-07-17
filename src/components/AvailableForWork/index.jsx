0848fe517b09b27aa3ce411acc891b15d975ebaf
rishiiiidha <rishidha04@gmail.com>
2024-07-17 16:32:04 +0530

    classname

--- src/components/AvailableForWork/index.jsx
+++ src/components/AvailableForWork/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div class="flex justify-center items-center w-[181px] h-[26px] bg-[#161616] text-white rounded-xl border-2 border-[#262626]">
-    <div class="size-2 bg-[#E63E21]  rounded-full">
+    <div className="flex justify-center items-center w-[181px] h-[26px] bg-[#161616] text-white rounded-xl border-2 border-[#262626]">
+    <div className="size-2 bg-[#E63E21]  rounded-full">
     </div>
-    <div class="uppercase text-[13px] px-1">
+    <div className="uppercase text-[13px] px-1">
     available for work
     </div>
     </div>
