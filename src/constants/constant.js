327cee6d22ad69570bdbc87bd281a8550871ad2a
rishiiiidha <rishidha04@gmail.com>
2024-07-20 18:59:04 +0530

    constants

---
+++ src/constants/constant.js
@@ -0,0 +1,50 @@
+export const Homepage = {
+  role: "Competitive Programmer & Full-stack Developer",
+  description:
+    "I'm Rishidha, a third-year student at SRM University AP, with a strong focus on problem-solving and backend logic. I enjoy building things using JavaScript, React, MongoDB, Express.js, and Node.js.",
+};
+
+export const identity = {
+  name: "Rishidha Addanki",
+  logo: "/me.jpeg",
+  email: "rishidha04@gmail.com",
+};
+
+export const aboutPageContent = {
+  now: "project I am currently working on ? ",
+  subtitle: "some information about myself",
+  about: {
+    description: `
+ I'm Rishidha, a third-year computer science student at SRM University AP with a passion for software development and problem-solving. I've gained hands-on experience through internship and projects. I'm actively involved in competitive programming and open-source contributions, constantly seeking to enhance my skills and contribute to impactful projects.`,
+    image: {
+      url: "/me.jpeg",
+      alt: "Right Picture",
+    },
+  },
+  work: {
+    description: `I interned as a Frontend Developer at Hygwell, using React, Hydrogen, Shopify, and Oxygen to develop a headless Shopify store. I also enhanced user experience with interactive features and app integrations. At Next Tech Lab, I participated in hackathon, where our team won first place in the 9Hacks hackathon with an AI Mock Interview Taker Application, showcasing our innovation and collaboration skills.`,
+    items: [
+      {
+        title: "Member",
+        company: {
+          name: "Next Tech Lab",
+          image: "/ntl.svg",
+          url: "https://srmap.edu.in/next_tech_lab/",
+        },
+        date: "Feb 2024 - Present",
+      },
+      {
+        title: "Frontend Developer Intern",
+        company: {
+          name: "Hygwell",
+          image: "/hygwell.png",
+          url: "https://hygwell.com/",
+        },
+        date: "May 2024 - Jun 2024",
+      },
+    ],
+  },
+  connect: {
+    description: `I'm always interested in meeting new people and learning new things. Feel free to connect with me.`, 
+  },
+};
