dd93d60ea2902b8b6a42b62fd628673788a91f34
rishiiiidha <rishidha04@gmail.com>
2024-09-10 14:20:53 +0530

    added resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,85 +10,78 @@ import { SiCodeforces } from "react-icons/si";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaDiscord } from "react-icons/fa";
+import { LinkPreview } from "./ui/link-preview";
+
+
+
 const AboutMe = () => {
-  return (
-    <section className="mt-10 max-w-2xl mx-auto px-6">
-      <div className="flex sm:flex-row   flex-col">
-        <div className="flex flex-col mb-4">
-          <h1 className="font-bold text-3xl mb-1">{identity.name}</h1>
-          <p className="opacity-60 mb-10">{Homepage.role}</p>
-          <div className="group flex justify-center gap-7 w-fit">
-            <div>
-              <a
-                href="https://github.com/rishiiiidha"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                target="_blank"
-              >
-                <FaGithub width={25} height={25} />
-              </a>
-            </div>
-            <div>
-              <a
-                href="/#"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                // target="_blank"
-              >
-                <SiLeetcode width={25} height={25} />
-              </a>
-            </div>
-            <div>
-              <a
-                href="/#"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                // target="_blank"
-              >
-                <SiCodechef width={25} height={25} />
-              </a>
-            </div>
-            <div>
-              <a
-                href="/#"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                // target="_blank"
-              >
-                <SiCodeforces width={25} height={25} />
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://www.linkedin.com/in/rishidha-addanki/"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                target="_blank"
-              >
-                <FaLinkedinIn width={25} height={25} />
-              </a>
-            </div>
-            <div>
-              <a
-                href="/#"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                // target="_blank"
-              >
-                <FaXTwitter width={25} height={25} />
-              </a>
-            </div>
-            <div>
-              <a
-                href="/#"
-                className="flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity"
-                // target="_blank"
-              >
-                <FaDiscord width={25} height={25} />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="h-screen w-full">
-          <Spline scene="https://prod.spline.design/NLx372xh4258Wxwq/scene.splinecode" />
-        </div>
+	return (
+		<section className='mt-10 max-w-2xl mx-auto px-6'>
+			<div className='flex sm:flex-row flex-col'>
+				<div className='flex flex-col mb-4'>
+					<h1 className='font-bold text-3xl mb-1'>{identity.name}</h1>
+					<p className='opacity-60 mb-10'>{Homepage.role}</p>
+					<div className='group flex justify-center gap-12 w-fit'>
+						<div>
+							<a
+								href='https://github.com/rishiiiidha'
+								className='flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity'
+								target='_blank'
+							>
+								<FaGithub width={25} height={25} />
+							</a>
+						</div>
+						<div>
+							<a
+								href='https://leetcode.com/u/rissh/'
+								className='flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity'
+								target='_blank'
+							>
+								<SiLeetcode width={25} height={25} />
+							</a>
+						</div>
+						<div>
+							<a
+								href='https://www.codechef.com/users/rishidhaa'
+								className='flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity'
+								target='_blank'
+							>
+								<SiCodechef width={25} height={25} />
+							</a>
+						</div>
+						<div>
+							<a
+								href='https://codeforces.com/profile/rishidhaa'
+								className='flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity'
+								target='_blank'
+							>
+								<SiCodeforces width={25} height={25} />
+							</a>
+						</div>
+						<div>
+							<a
+								href='https://www.linkedin.com/in/rishidha-addanki/'
+								className='flex items-center gap-2 opacity-60 group-hover:opacity-20 hover:!opacity-100 transition-opacity'
+								target='_blank'
+							>
+								<FaLinkedinIn width={25} height={25} />
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className='h-screen w-full'>
+					<div className='h-screen w-full flex xs:flex-col'>
+						<div>
+							<Spline scene='https://prod.spline.design/NLx372xh4258Wxwq/scene.splinecode' />
+						</div>
+					</div>
+				</div>
+				
       </div>
-    </section>
-  );
+      
+		</section>
+	);
 };
 
 export default AboutMe;
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { MdEmail } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { identity } from "../constants/constant";
+import { LinkPreview } from "./ui/link-preview";
 
 const navBarLinks = [
   { title: "Home", url: "/" },
@@ -9,6 +10,45 @@ const navBarLinks = [
   { title: "Projects", url: "/projects" },
   { title: "Achievements", url: "/achievements" },
 ];
+
+const RotatingText = ({ text, duration = 10, size = 50, fontSize = 9 }) => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox={`0 0 ${size} ${size}`}
+			width={size}
+			height={size}
+		>
+			<defs>
+				<path
+					id='circle-path'
+					d={`M${size / 2},${size / 2} m-${size / 3},0 a${size / 3},${
+						size / 3
+					} 0 1,1 ${(size * 2) / 3},0 a${size / 3},${size / 3} 0 1,1 -${
+						(size * 2) / 3
+					},0`}
+				/>
+			</defs>
+			<text
+				fontFamily='Arial, sans-serif'
+				fontSize={fontSize}
+				fill='currentColor'
+			>
+				<textPath href='#circle-path' startOffset='0%'>
+					<animate
+						attributeName='startOffset'
+						from='0%'
+						to='100%'
+						begin='0s'
+						dur={`${duration}s`}
+						repeatCount='indefinite'
+					/>
+					{`${text} • ${text} • ${text} • ${text}`}
+				</textPath>
+			</text>
+		</svg>
+	);
+};
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -32,69 +72,80 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="px-6 py-6 max-w-2xl mx-auto flex items-center justify-between">
-      <NavLink to="/">
-        <img
-          src={identity.logo}
-          alt={"Logo"}
-          width={50}
-          height={50}
-          className="w-[50px] h-[50px] object-cover rounded-full"
-        />
-      </NavLink>
-      <nav className="ml-auto mr-6 sm:m-0">
-        <ul className="hidden sm:flex sm:items-center sm:gap-2">
-          {navBarLinks.map((link) => (
-            <li key={link.url}>
-              <NavLink
-                className="group px-4 py-2 rounded-lg hover:bg-gray-bg opacity-60 hover:opacity-100 transition-all"
-                to={link.url}
-              >
-                {link.title}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-        <div className="relative sm:hidden" ref={menuRef}>
-          <button
-            id="link-menu-button"
-            className="relative font-medium opacity-60 p-4"
-            onClick={toggleMenu}
-          >
-            Menu <span id="open-indicator">{menuOpen ? "-" : "+"}</span>
-          </button>
-          <div
-            id="link-menu"
-            className={`absolute p-2 right-0 bg-black  border-slate-900 rounded-lg w-44 transition-opacity ease-in-out duration-300 ${
-              menuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-            } z-40`}
-          >
-            <ul>
-              {navBarLinks.map((link) => (
-                <li key={link.url}>
-                  <NavLink
-                    className="block pl-4 py-3 opacity-100"
-                    to={link.url}
-                  >
-                    {link.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div>
-        <a
-          href={`mailto:${`rishidha04@gmail.com`}`}
-          className="block p-2 opacity-60 hover:opacity-100 transition-opacity"
-          aria-label="Email"
-        >
-          <MdEmail name="mdi:email" width={20} height={20} />
-        </a>
-      </div>
-    </div>
-  );
+		<div className='px-6 py-6 max-w-2xl mx-auto flex items-center justify-between'>
+			<NavLink to='/'>
+				<img
+					src={identity.logo}
+					alt={"Logo"}
+					width={50}
+					height={50}
+					className='w-[50px] h-[50px] object-cover rounded-full'
+				/>
+			</NavLink>
+			<nav className='ml-auto mr-6 sm:m-0'>
+				<ul className='hidden sm:flex sm:items-center sm:gap-2'>
+					{navBarLinks.map((link) => (
+						<li key={link.url}>
+							<NavLink
+								className='group px-4 py-2 rounded-lg hover:bg-gray-bg opacity-60 hover:opacity-100 transition-all'
+								to={link.url}
+							>
+								{link.title}
+							</NavLink>
+						</li>
+					))}
+				</ul>
+				<div className='relative sm:hidden' ref={menuRef}>
+					<button
+						id='link-menu-button'
+						className='relative font-medium opacity-60 p-4'
+						onClick={toggleMenu}
+					>
+						Menu <span id='open-indicator'>{menuOpen ? "-" : "+"}</span>
+					</button>
+					<div
+						id='link-menu'
+						className={`absolute p-2 right-0 bg-black  border-slate-900 rounded-lg w-44 transition-opacity ease-in-out duration-300 ${
+							menuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+						} z-40`}
+					>
+						<ul>
+							{navBarLinks.map((link) => (
+								<li key={link.url}>
+									<NavLink
+										className='block pl-4 py-3 opacity-100'
+										to={link.url}
+									>
+										{link.title}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<div>
+				<a
+					href={`mailto:${`rishidha04@gmail.com`}`}
+					className='block p-2 opacity-60 hover:opacity-100 transition-opacity'
+					aria-label='Email'
+				>
+					<MdEmail name='mdi:email' width={20} height={20} />
+				</a>
+			</div>
+			<div className="mt-2" >
+				<LinkPreview
+					url='https://drive.google.com/file/d/1RxTfkWOooKihiXhmAqfoDCVxg96lTN1M/view'
+					imageSrc='/resume-sc.png'
+					isStatic
+					className='relative mt-10 bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500'
+				>
+					<RotatingText text='Resume' />
+				</LinkPreview>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
